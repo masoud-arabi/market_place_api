019accce07a1b236168411d5143a1c7b389a4537
masoud arabi <applepie@masouds-MacBook-Air.local>
2023-02-26 19:02:30 -0500

    add serilezer to user

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+
   test "should have a positive price" do
     product = products(:one) 
     product.price = -1 
     assert_not product.valid?
   end
+
+  test "should filter products by name" do
+    assert_equal 2, Product.filter_by_title('tv').count
+  end
+  
+  test 'should filter products by name and sort them' do 
+    assert_equal [products(:another_tv), products(:one)],
+    Product.filter_by_title('tv').sort 
+  end
+
+  test "should filter products by price equal or above" do
+    assert_equal 2, Product.above_or_equal_to_price(200).count
+    assert_equal [products(:two), products(:one)],
+    Product.above_or_equal_to_price(200).sort
+  end
+
+  test "should filter products by price equal or lower" do
+    assert_equal 2, Product.lower_or_equal_to_price(200).count
+    assert_equal [products(:another_tv), products(:last_tv)],
+    Product.lower_or_equal_to_price(200)
+  end
+
 end
