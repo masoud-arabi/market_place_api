58362af21c6509fc8ad0645eb0a1ff5c0ed76e51
masoud arabi <applepie@masouds-MacBook-Air.local>
2023-02-27 19:41:51 -0500

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::OrdersController < ApplicationController
-    before_action :check_login, only: %i[index]
+    before_action :check_login, only: %i[index show]
 
     def index 
         @orders = OrderSerializer.new(current_user.orders
         ).serializable_hash
         render json: @orders, status: 200
+    end
+    
+    def show 
+        @order = current_user.orders.find(params[:id])
+        if @order
+            options = {include: [:products]}
+            @order = OrderSerializer.new(@order, options
+            ).serializable_hash
+            render json: @order, status: 200
+        else
+            head 402
+        end
     end
 end
